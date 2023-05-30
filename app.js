71e00996f5428ddd5382cb23f956ad4e7246ee3f
JCouderc87 <jeremy.couderc@yahoo.fr>
2023-05-30 16:37:33 +0200

    first push

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.get("/", function(req, res, next){
+    console.log("we have received a request for the HOMEPAGE");
+    res.sendFile(__dirname + "/views/home-page.html");
+});
+
+// GET /contact
+app.get("/about", (req, res, next) => {
+    res.sendFile(__dirname + "/views/about-page.html");
+});
+
+
+
+
+
+
+app.listen(3000);
